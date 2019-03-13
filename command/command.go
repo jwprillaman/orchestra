8c71ee514a071df48f19e2be3d8b9daaf124a4bd
James Prillaman <jwprillaman@gmail.com>
2019-03-12 22:12:13 -0600

    created songs command

--- command/command.go
+++ command/command.go
@@ -20,13 +20,28 @@ func Run(address string, command string, input string) {
 		defer conn.Close()
 		c := directorProto.NewDirectorClient(conn)
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-		p, err := c.GetPlayers(ctx, &directorProto.Filter{PlayerName: "", SongIds: ""})
+		p, err := c.GetPlayers(ctx, &directorProto.Filter{})
 		defer cancel()
 
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(p)
+	case "songs":
+		conn, err := grpc.Dial(address, grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("did not connect: %v\n", err)
+		}
+		defer conn.Close()
+		c := directorProto.NewDirectorClient(conn)
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		s, err := c.GetSongs(ctx, &directorProto.Filter{})
+		defer cancel()
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(s)
 	default:
 		fmt.Printf("Invalid command : %v\n")
 	}
